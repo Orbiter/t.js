a2e1545d6758a5017536f2a91034b62407a299ee
Michael Peter Christen <mc@yacy.net>
2022-01-13 11:36:11 +0100

    enhanced code, made it more compact and readable

--- t.js
+++ t.js
@@ -16,7 +16,7 @@
 	@version 0.1.0
 
 */
-(function() {
+(function () {
 
 	var blockregex = /\{\{(([@!]?)(.+?))\}\}(([\s\S]+?)(\{\{:\1\}\}([\s\S]+?))?)\{\{\/\1\}\}/g,
 		valregex = /\{\{([=%])(.+?)\}\}/g,
@@ -26,126 +26,70 @@
 		this.t = template;
 	}
 
-	function scrub(val) {
-		return new Option(val).innerHTML.replace(/"/g,"&quot;");
-	}
-
 	function get_value(vars, key) {
-		var parts = key.split('.');
-		while (parts.length) {
-			if (!(parts[0] in vars)) {
-				return false;
-			}
+        for (parts = key.split("."); parts.length;) {
+			if (!(parts[0] in vars)) return false;
 			vars = vars[parts.shift()];
 		}
 		return vars;
 	}
 
-	function get_value_con(vars, key){
+	function get_value_con(vars, key) {
 		var v = null;
-		if(key.charAt(0) == "'" || key.charAt(0) == '"'){
+		if ("'" == key.charAt(0) || '"' == key.charAt(0)) {
 			v = key.substr(1);
-			if(v.charAt(v.length - 1) == "'" || v.charAt(v.length - 1) == '"') {
-				v = v.substr(0,v.length -1);
-			}
-		}else if(!isNaN(key.charAt(0))){
-			v = Number(key);
-		}else{
-			v = get_value(vars, key);
-		}
+			if ("'" == v.charAt(v.length - 1) || '"' == v.charAt(v.length - 1))	v = v.substr(0, v.length - 1);
+		} else v = isNaN(key.charAt(0)) ? get_value(vars, key) : Number(key);
 		return v;
 	}
 
 	function render(fragment, vars) {
 		return fragment
-			.replace(blockregex, function(_, __, meta, key, inner, if_true, has_else, if_false) {
-
-				var val = get_value(vars,key), temp = "", i;
-
+			.replace(blockregex, function (_, __, meta, key, inner, if_true, has_else, if_false) {
+				var val = get_value(vars, key), temp = "", i;
 				if (!val) {
-
 					// handle if not
-					if (meta == '!') {
-					    return render(inner, vars);
-					}else{
-						var consp = conregex.exec(key);
-						if(consp){
-							var l = get_value_con(vars,consp[1]);
-							var r = get_value_con(vars,consp[3]);
-							var op = consp[2].replace(/\s/g,'');
-							var conV = false;
-							switch(op){ //do not use the eval(), it is not safe
-								case '==':
-									conV = (l == r);
-									break;
-								case '===':
-									conV = (l === r);
-									break;
-								case '>':
-									conV = (l > r);
-									break;
-								case '<':
-									conV = (l < r);
-									break;
-								case '>=':
-									conV = (l >= r);
-									break;
-								case '<=':
-									conV = (l <= r);
-								case '!=':
-									conV = (l != r);
-									break;
-							}
+					if ('!' == meta) return render(inner, vars);
 
-							if(conV){
-								return render(inner, vars);
-							}else{
-								//not support	
-							}
-						}
-					}
+                    if (consp = conregex.exec(key)) {
+                        var l = get_value_con(vars,consp[1]);
+                        var r = get_value_con(vars,consp[3]);
+                        var conV = false;
+                        switch(consp[2].replace(/\s/g,'')) { //do not use the eval(), it is not safe
+                            case '==':  conV = (l == r);  break;
+                            case '===': conV = (l === r); break;
+                            case '>':   conV = (l > r);   break;
+                            case '<':   conV = (l < r);   break;
+                            case '>=':  conV = (l >= r);  break;
+                            case '<=':  conV = (l <= r);  break;
+                            case '!=':  conV = (l != r);  break;
+                        }
+                        if (conV) return render(inner, vars);
+                    }
+
 					// check for else
-					if (has_else) {
-						return render(if_false, vars);
-					}
-
-					return "";
+                    return has_else ? render(if_false, vars) : "";
 				}
 
 				// https://github.com/jasonmoo/t.js/issues/10
-				if (meta == '!') return "";
+				if ('!' == meta) return "";
 
 				// regular if
-				if (!meta) {
-					return render(if_true, vars);
-				}
+				if (!meta) return render(if_true, vars);
 
 				// process array/obj iteration
-				if (meta == '@') {
+				if ('@' == meta) {
 					// store any previous vars
 					// reuse existing vars
 					_ = vars._key;
 					__ = vars._val;
-					for (i in val) {
-						if (val.hasOwnProperty(i)) {
-							vars._key = i;
-							vars._val = val[i];
-							temp += render(inner, vars);
-						}
-					}
+					for (i in val) val.hasOwnProperty(i) && (vars._key = i,	vars._val = val[i],	temp += render(inner, vars));
 					vars._key = _;
 					vars._val = __;
 					return temp;
 				}
-
-			})
-			.replace(valregex, function(_, meta, key) {
-				var val = get_value(vars,key);
-
-				if (val || val === 0) {
-					return meta == '%' ? scrub(val) : val;
-				}
-				return "";
+			}).replace(valregex, function(_, meta, key) {
+				return (val = get_value(vars,key)) || 0 === val ? (meta == '%' ? new Option(val).innerHTML.replace(/"/g, "&quot;") : val) : "";
 			});
 	}
 
@@ -154,5 +98,4 @@
 	};
 
 	window.t = t;
-
 })();
